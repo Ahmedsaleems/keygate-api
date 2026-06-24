79e47733e41944d01e91d8d1e2459714e07b4ae7
Ahmedsaleems <ahmedsaleems123@gmail.com>
2026-06-24 17:50:14 +0500

    fix(gateway-application): include missing application error contracts

--- src/modules/gateway/application/error.ts
+++ src/modules/gateway/application/error.ts
@@ -8,3 +8,50 @@ export class GatewayApiKeyMissingError extends ApplicationError {
   }
 }
 
+export class GatewayApiKeyInvalidError extends ApplicationError {
+  readonly code = 'GATEWAY_API_KEY_INVALID';
+
+  constructor() {
+    super('API key is invalid.');
+  }
+}
+
+export class GatewayApiKeyRevokedError extends ApplicationError {
+  readonly code = 'GATEWAY_API_KEY_REVOKED';
+
+  constructor() {
+    super('API key has been revoked.');
+  }
+}
+
+export class GatewayEndpointNotFoundError extends ApplicationError {
+  readonly code = 'GATEWAY_ENDPOINT_NOT_FOUND';
+
+  constructor() {
+    super('Gateway endpoint was not found.');
+  }
+}
+
+export class GatewayEndpointDisabledError extends ApplicationError {
+  readonly code = 'GATEWAY_ENDPOINT_DISABLED';
+
+  constructor() {
+    super('Gateway endpoint is disabled.');
+  }
+}
+
+export class GatewayEndpointAccessDeniedError extends ApplicationError {
+  readonly code = 'GATEWAY_ENDPOINT_ACCESS_DENIED';
+
+  constructor() {
+    super('API key does not have access to this endpoint.');
+  }
+}
+
+export class GatewayUpstreamRequestFailedError extends ApplicationError {
+  readonly code = 'GATEWAY_UPSTREAM_REQUEST_FAILED';
+
+  constructor(message = 'Gateway upstream request failed.') {
+    super(message);
+  }
+}
